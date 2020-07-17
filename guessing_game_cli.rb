66aff40a31db0116c1b2b12047d2ff26c94c7e3d
Natalia Galán <n8galan@gmail.com>
2020-07-17 02:03:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,14 +7,9 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input != random
     puts "Sorry! The computer guessed #{random}."
+  elsif input == 'exit'
+    print "Goodbye!"
   end
 
 end
 
-input == 'exit'
-  print "Goodbye!"
-
-
-
-If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
-It the user's input is equal to "exit": Goodbye!
