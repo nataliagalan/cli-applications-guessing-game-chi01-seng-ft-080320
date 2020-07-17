862087b359530264e7917b882e94dda11ecff38e
Natalia Galán <n8galan@gmail.com>
2020-07-17 02:06:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     print "Goodbye!"
   elsif input != random
     print "Sorry! The computer guessed #{random}."
-  elsif 
+  elsif
     input == random
     print "You guessed the correct number!"
   end
